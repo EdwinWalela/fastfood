3575afdcbcd8db1b1a83548ef048199d11308614
edwinwalela <edwinwalela@gmail.com>
2022-12-27 11:35:41 +0300

    footer

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,26 +1,30 @@
+import Footer from "../Layout/Footer"
 import BookTable from "./bookTable"
 
 function LandingPage(){
   return (
-    <div className="py-2 px-28">
-      <div className="flex">
-        <div className="flex-auto">
-          <h3 className="font-medium my-2">EASY WAY TO ORDER YOUR FOOD</h3>
-          <h1 className="text-6xl font-bold">
-            Choose Healthy <br/>& Fresh Food
-          </h1>
-          <p className="my-2">
-            Just confirm your order and enjoy our delicious fastest delivery.
-          </p>
-          <button className="bg-black text-white py-3 px-5 rounded-lg my-2">Order Now</button>
-          <button className="py-3 px-5 rounded-lg my-2 mx-6">See Menu</button>
+    <>
+      <div className="py-2 px-28 mb-8">
+        <div className="flex">
+          <div className="flex-auto">
+            <h3 className="font-medium my-2">EASY WAY TO ORDER YOUR FOOD</h3>
+            <h1 className="text-6xl font-bold">
+              Choose Healthy <br/>& Fresh Food
+            </h1>
+            <p className="my-2">
+              Just confirm your order and enjoy our delicious fastest delivery.
+            </p>
+            <button className="bg-black text-white py-3 px-5 rounded-lg my-2">Order Now</button>
+            <button className="py-3 px-5 rounded-lg my-2 mx-6">See Menu</button>
+          </div>
+          <div className="flex-auto">
+            <img className="w-80 mx-auto block" src="/assets/img/food-landing.png"/>
+          </div>
         </div>
-        <div className="flex-auto">
-          <img className="w-80 mx-auto block" src="/assets/img/food-landing.png"/>
-        </div>
+        <BookTable />
       </div>
-      <BookTable />
-    </div>
+      <Footer />
+    </>
   )
 }
 
