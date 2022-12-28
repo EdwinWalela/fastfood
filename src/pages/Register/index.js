d250a655e1068b0ee9e7fbfe048a719f7ce965ba
edwinwalela <edwinwalela@gmail.com>
2022-12-29 00:02:09 +0300

    register form hooks

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,4 +1,6 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { useSelector, useDispatch } from 'react-redux'
+import { registerUser } from './registerSlice'
 
 const RegisterPage = () => {
   
@@ -6,6 +8,39 @@ const RegisterPage = () => {
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
   const [termsAndCondition,setTermsAndConditions] = useState(false)
+  const [passwordValid, setPasswordValid] = useState(false)
+  const [passwordError, setPasswordError] = useState('')
+  const [emailValid, setEmailError] = useState(false)
+
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    if(password.length<5){
+      setPasswordValid(false);
+      setPasswordError('Password should be at least 5 characters long')
+    }else{
+      setPasswordError('')
+      setPasswordValid(true)
+    }
+  },[password])
+
+  useEffect(()=>{
+    if(password != confirmPassword){
+      setPasswordValid(false)
+      setPasswordError('Passwords do not match')
+    }else{
+      setPasswordValid(true)
+      setPasswordError('')
+    }
+  },[confirmPassword])
+
+  useEffect(()=>{
+    if(email.includes('@') && email.includes('.')){
+      setEmailError(true)
+    }else{
+      setEmailError(false)
+    }
+  },[email])
 
   function updatePassword(e){
     setPassword(e.target.value);
@@ -16,11 +51,19 @@ const RegisterPage = () => {
   }
 
   function updateConfirmPassword(e){
-    setConfirmPassword(e.target.value)
+    setConfirmPassword(e.target.value);
   }
 
   function updateTermsAndConditions(){
     setTermsAndConditions(isChecked=>isChecked ? false:true)
+  }
+
+  function submitForm(e){
+    e.preventDefault();
+    dispatch(registerUser({
+      email,
+      password
+    }));
   }
 
   
@@ -33,18 +76,25 @@ const RegisterPage = () => {
                     <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                         Create an account
                     </h1>
-                    <form className="space-y-4 md:space-y-6" action="#">
+                    <form className="space-y-4 md:space-y-6" action="#" onSubmit={submitForm}>
                         <div>
                             <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                             <input type="email" name="email" id="email" value={email} onChange={updateEmail} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
+                            {!emailValid && 
+                              <pre className="text-xs my-2 text-red-700">Please enter a valid email address</pre>
+                            }
                         </div>
                         <div>
                             <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                             <input type="password" name="password" id="password" value={password} onChange={updatePassword}  placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                            {!passwordValid && 
+                              <pre className="text-xs my-2 text-red-700">{passwordError}</pre>
+                            }
                         </div>
                         <div>
                             <label for="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
                             <input type="password" name="confirm-password" id="confirm-password" value={confirmPassword} onChange={updateConfirmPassword} placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                           
                         </div>
                         <div className="flex items-start">
                             <div className="flex items-center h-5">
--- src/pages/Register/registerSlice.js
+++ src/pages/Register/registerSlice.js
@@ -9,8 +9,13 @@ export const registerSlice = createSlice({
   name:'register-user',
   initialState,
   reducers:{
-    registerUser:(state)=>{
-      console.log(state)
+    registerUser:(state,action)=>{
+      let {email,password} = action.payload;
+
+      state.email = email;
+      state.password = password
+
+      // submit data to API
     }
   }
 })
