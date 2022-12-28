ecbfe78ebbf67a8a7f1b1981e1f30683ec8b6b9f
edwinwalela <edwinwalela@gmail.com>
2022-12-28 12:31:23 +0300

    register page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import NavBar from './pages/Layout/NavBar';
 import LandingPage from './pages/Landing';
+import RegisterPage from './pages/Register';
 import Footer from "./pages/Layout/Footer"
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <LandingPage />
+      <RegisterPage />
+      {/* <LandingPage /> */}
       <Footer />
     </div>
   );
