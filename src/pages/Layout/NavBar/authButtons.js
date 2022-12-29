a9c19b7f08307285010b4b590112d30150495ac8
edwinwalela <edwinwalela@gmail.com>
2022-12-29 21:19:32 +0300

    authentication routes

--- src/pages/Layout/NavBar/authButtons.js
+++ src/pages/Layout/NavBar/authButtons.js
@@ -1,12 +1,18 @@
-function AuthButtons(){
+import { Link } from 'react-router-dom'
+
+const AuthButtons = () =>{
   return (
     <div className=" ">
-      <button className="m-1 py-3 px-5 rounded-lg">
-        Sign in
-      </button>
-      <button className="m-1 bg-black text-white py-3 px-5 rounded-lg">
-        Sign Up
-      </button>
+      <Link to="/sign-in">
+        <button className="m-1 py-3 px-5 rounded-lg">
+          Sign in
+        </button>
+      </Link>
+      <Link to="/register">
+        <button className="m-1 bg-black text-white py-3 px-5 rounded-lg">
+          Sign Up
+        </button>
+        </Link>
     </div>
   )
 }
