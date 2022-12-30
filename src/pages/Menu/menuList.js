1d95f566af48351f474e7b365e0a394d06e59de3
edwinwalela <edwinwalela@gmail.com>
2022-12-30 20:22:20 +0300

    render state in menu list

--- src/pages/Menu/menuList.js
+++ src/pages/Menu/menuList.js
@@ -4,7 +4,7 @@ const MenuList = (props) =>{
   console.log(props.menu)
   return (
     <div className="mt-2 mb-8 px-14 flex flex-wrap">
-        {props.menu.map((item,k)=><MenuListItem key={k} menuItem={item}/>)}
+        {props.menu.map((item,k)=><MenuListItem key={k} item={item}/>)}
        
     </div>
   )
--- src/pages/Menu/menuListItem.js
+++ src/pages/Menu/menuListItem.js
@@ -1,10 +1,10 @@
-const MenuListItem = () =>{
+const MenuListItem = (props) =>{
   return (
     <div className="shadow-md bg-white py-6 mx-8 my-4 text-center w-80 rounded-lg">
-      <h3 className="text-left bg-black text-white pl-6 py-1 rounded-tr-md rounded-br-md w-1/3">KES 300</h3>
+      <h3 className="text-left bg-black text-white pl-6 py-1 rounded-tr-md rounded-br-md w-1/3">KES {props.item.price}</h3>
       <img src="/assets/img/food-landing.png" className="px-8 my-4 w-48 block m-auto" alt=""/>
-      <h2 className="text-lg font-medium px-8 ">Greek Salad</h2>
-      <p className="text-xs px-8 ">Quality Greek kalamata olives and creamy feta cheese, made from sheep’s milk</p>
+      <h2 className="text-lg font-medium px-8 ">{props.item.title}</h2>
+      <p className="text-xs px-8 ">{props.item.description}.</p>
       <button className="bg-black text-white py-1  px-6 mx-6 rounded-full my-4">Add To Cart</button>
     </div>
   )
