ced07a7916488a2f7af919b6a8d0a5a692c374a3
edwinwalela <edwinwalela@gmail.com>
2022-12-30 17:02:50 +0300

    add NavLink component to navigation bar

--- src/pages/Layout/NavBar/menuItems.js
+++ src/pages/Layout/NavBar/menuItems.js
@@ -1,16 +1,24 @@
-function MenuItems(){
+import { NavLink } from "react-router-dom"
+
+const MenuItems = () =>{
   return (
     <div className="flex-1 ml-16">
       <ul className="flex justify-center">
-        <li className="px-6 py-3 cursor-pointer">
-          Find Food
-        </li>
-        <li className="px-6 py-3 font-medium cursor-pointer">
-          Find Restaurants
-        </li>
-        <li className="px-6 py-3 cursor-pointer">
+        <NavLink 
+          to="/about-us" 
+          className={({isActive})=> isActive ? "px-6 py-3 font-medium cursor-pointer": "px-6 py-3 cursor-pointer"}>
+          About Us
+        </NavLink>
+        <NavLink 
+          to="/menu" 
+          className={({isActive})=> isActive ? "px-6 py-3 font-medium cursor-pointer": "px-6 py-3 cursor-pointer"}>
+          Our Menu
+        </NavLink>
+        <NavLink 
+          to="/contact-us" 
+          className={({isActive})=> isActive ? "px-6 py-3 font-medium cursor-pointer": "px-6 py-3 cursor-pointer"}>
           Contact Us
-        </li>
+        </NavLink>
       </ul>
     </div>
   )
