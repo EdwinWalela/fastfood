8529cea483785c49c1d155f40678c6572f2de716
edwinwalela <edwinwalela@gmail.com>
2022-12-30 16:57:08 +0300

    separate menu list  into seperate component

---
+++ src/pages/Menu/menuList.js
@@ -0,0 +1,13 @@
+import MenuListItem from "./menuListItem";
+
+const MenuList = () =>{
+  return (
+    <div className="mt-2 mb-8 px-14 flex flex-wrap">
+        <MenuListItem />
+        <MenuListItem />
+        <MenuListItem />
+    </div>
+  )
+}
+
+export default MenuList;
