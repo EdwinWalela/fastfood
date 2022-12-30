edef8b43c80a1712379147e28b066243035ab8d7
edwinwalela <edwinwalela@gmail.com>
2022-12-30 19:59:26 +0300

    add menu reducer to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import registerReducer from '../pages/Register/registerSlice';
 import loginReducer from '../pages/Login/loginSlice';
-import tableBookingReducer from "../pages/Landing/tableBookingSlice";
+import tableBookingReducer from "../pages/Landing/tableBookingSlice"
+import menuReducer from "../pages/Menu/menuSlice";
 
 export const store = configureStore({
   reducer:{
     registerUser:registerReducer,
     login:loginReducer,
-    booking:tableBookingReducer
+    booking:tableBookingReducer,
+    menu:menuReducer
   }
 })
