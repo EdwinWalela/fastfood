1a90793ee8bebd60748af4f64f57a9cb9fcffe0f
edwinwalela <edwinwalela@gmail.com>
2023-01-02 23:26:30 +0300

    add prettier

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,17 +1,17 @@
-import CartList from "./CartList"
-import OrderSummary from "./summary"
+import CartList from './CartList';
+import OrderSummary from './summary';
 
-const CartPage = () =>{
-  return (
-    <div className="m-h-screen">
-      <div className="mx-auto my-8 p-10 rounded-lg shadow-lg w-2/5 bg-white">
-        <h3 className="font-bold text-md my-2 text-gray-500">YOUR ORDER</h3>
-        <div className="border my-4"></div>
-        <CartList />
-        <OrderSummary />
-      </div>
-    </div>
-  )
-}
+const CartPage = () => {
+	return (
+		<div className="m-h-screen">
+			<div className="mx-auto my-8 p-10 rounded-lg shadow-lg w-2/5 bg-white">
+				<h3 className="font-bold text-md my-2 text-gray-500">YOUR ORDER</h3>
+				<div className="border my-4"></div>
+				<CartList />
+				<OrderSummary />
+			</div>
+		</div>
+	);
+};
 
-export default CartPage
+export default CartPage;
