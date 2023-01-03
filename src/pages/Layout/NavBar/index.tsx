a4b6bf7e8db80166215240c71ca2a3e57129a1b4
edwinwalela <edwinwalela@gmail.com>
2023-01-03 13:46:07 +0300

    select cart length from navbar

--- src/pages/Layout/NavBar/index.tsx
+++ src/pages/Layout/NavBar/index.tsx
@@ -1,15 +1,18 @@
-import AuthButtons from "./authButtons"
-import Logo from "./logo"
-import MenuItems from "./menuItems"
+import AuthButtons from './authButtons';
+import Logo from './logo';
+import MenuItems from './menuItems';
+import { useAppSelector } from '../../../store/hooks';
+import { stat } from 'fs';
 
 const NavBar = () => {
-  return (
-    <div className="p-4 flex">
-      <Logo />
-      <MenuItems cartCount={2}/>
-      <AuthButtons />
-    </div>
-  )
-}
+	const cart = useAppSelector((state) => state.cart.items);
+	return (
+		<div className="p-4 flex">
+			<Logo />
+			<MenuItems cartCount={cart.length} />
+			<AuthButtons />
+		</div>
+	);
+};
 
-export default NavBar
+export default NavBar;
