805b7adcf65b667e01587669088804ff7dd13624
edwinwalela <edwinwalela@gmail.com>
2023-01-03 13:26:08 +0300

    add cart reducer to store

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,17 +1,19 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
 import registerReducer from '../pages/Register/registerSlice';
 import loginReducer from '../pages/Login/loginSlice';
-import tableBookingReducer from "../pages/Landing/tableBookingSlice"
-import menuReducer from "../pages/Menu/menuSlice";
+import tableBookingReducer from '../pages/Landing/tableBookingSlice';
+import menuReducer from '../pages/Menu/menuSlice';
+import cartReducer from '../pages/Cart/cartSlice';
 
 export const store = configureStore({
-  reducer:{
-    registerUser:registerReducer,
-    login:loginReducer,
-    booking:tableBookingReducer,
-    menu:menuReducer
-  }
-})
+	reducer: {
+		registerUser: registerReducer,
+		login: loginReducer,
+		booking: tableBookingReducer,
+		menu: menuReducer,
+		cart: cartReducer,
+	},
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
