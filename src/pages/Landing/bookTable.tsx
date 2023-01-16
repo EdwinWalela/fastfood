7b142f26c51d9b26c0fd81c8f98619b933671dfd
edwinwalela <edwinwalela@gmail.com>
2023-01-16 13:56:50 +0300

    improve landing page responsiveness

--- src/pages/Landing/bookTable.tsx
+++ src/pages/Landing/bookTable.tsx
@@ -1,115 +1,144 @@
-import React, { useState, useEffect } from 'react'
-import { bookTable } from './tableBookingSlice'
-import { useAppDispatch } from '../../store/hooks'
-import { useAppSelector } from '../../store/hooks'
+import React, { useState, useEffect } from 'react';
+import { bookTable } from './tableBookingSlice';
+import { useAppDispatch } from '../../store/hooks';
+import { useAppSelector } from '../../store/hooks';
 
-const BookTable = () =>{
-  const [date, setDate] = useState(()=>{
-    const now = new Date();
-    now.setDate(now.getDate()+1);
-    return now.toISOString().substring(0,10);
-  })
-  const [time, setTime] = useState('13:00');
-  const [email, setEmail] = useState('');
-  const [dateValid, setDateValid] = useState(true);
-  const [timeValid, setTimeValid] = useState(true);
-  const [emailValid, setEmailValid] = useState(true);
+const BookTable = () => {
+	const [date, setDate] = useState(() => {
+		const now = new Date();
+		now.setDate(now.getDate() + 1);
+		return now.toISOString().substring(0, 10);
+	});
+	const [time, setTime] = useState('13:00');
+	const [email, setEmail] = useState('');
+	const [dateValid, setDateValid] = useState(true);
+	const [timeValid, setTimeValid] = useState(true);
+	const [emailValid, setEmailValid] = useState(true);
 
-  const dispatch = useAppDispatch();
+	const dispatch = useAppDispatch();
 
-  useEffect(()=>{
-    let selectedDate = new Date(date);
-    let now = new Date();
-    if(selectedDate<now){
-      setDateValid(false)
-    }else{
-      setDateValid(true)
-    }
-  }
-  ,[date])
-  
-  useEffect(()=>{
-    let hour = Number(time.split(':')[0])
-    if(hour<13 || hour > 22){
-      setTimeValid(false)
-    }else{
-      setTimeValid(true)
-    }
-  }
-  ,[time])
-  
-  useEffect(()=>{
-   if(email){
-    if(email.includes('@') && email.includes('.')){
-      setEmailValid(true)
-    }else{
-      setEmailValid(false)
-    }
-   }
-  },[email])
+	useEffect(() => {
+		let selectedDate = new Date(date);
+		let now = new Date();
+		if (selectedDate < now) {
+			setDateValid(false);
+		} else {
+			setDateValid(true);
+		}
+	}, [date]);
 
-  function updateDate(e:React.FormEvent<HTMLInputElement>){
-    setDate(e.currentTarget.value);
-  }
+	useEffect(() => {
+		let hour = Number(time.split(':')[0]);
+		if (hour < 13 || hour > 22) {
+			setTimeValid(false);
+		} else {
+			setTimeValid(true);
+		}
+	}, [time]);
 
-  function updateTime(e:React.FormEvent<HTMLInputElement>){
-    setTime(e.currentTarget.value);
-  }
+	useEffect(() => {
+		if (email) {
+			if (email.includes('@') && email.includes('.')) {
+				setEmailValid(true);
+			} else {
+				setEmailValid(false);
+			}
+		}
+	}, [email]);
 
-  function updateEmail(e:React.FormEvent<HTMLInputElement>){
-    setEmail(e.currentTarget.value);
-  }
+	function updateDate(e: React.FormEvent<HTMLInputElement>) {
+		setDate(e.currentTarget.value);
+	}
 
-  function submitForm(e:React.FormEvent){
-    e.preventDefault();
-    if(!dateValid){
-      alert('Booking date should be at least one day in advance');
-    }else if(!timeValid){
-      alert('Booking time should be between 1.00 PM and 10.00 PM');
-    }else if(!emailValid || !email){
-      alert('Please provide a valid email before booking a table');
-    }else{
-      dispatch(bookTable({
-        date,
-        email,
-        time
-      }))
-    }
-  }
-  
-  return (
-    <div className="mb-12">
-      <h2 className="mb-4 font-medium text-lg">Book A Table</h2>
-      <form className="w-11/12 bg-white rounded-md shadow-lg py-4" action="#" onSubmit={submitForm}>
-        {!dateValid && 
-          <pre className='p-2 text-center text-sm text-red-500'>Your booking date should be at least one day in advance.</pre>
-        }
-        {!timeValid &&
-          <pre className='p-2 text-center text-sm text-red-500'>Booking time should be between 1.00 PM and 10.00 PM EAT</pre>
-        }
-        {!emailValid &&
-        <pre className='p-2 text-center text-sm text-red-500'>Please provide a valid email address</pre>
-        }
-        <div className="flex justify-center p-4">
-          <div className="justify-center-auto mr-8">
-            <label className="block font-medium">Date</label>
-            <input type="date" value={date} onChange={updateDate} className="block py-2 px-4 border-2"/>
-          </div>
-          <div className="justify-center-auto mr-8">
-            <label className="block font-medium">Time</label>
-            <input type="time" value={time} onChange={updateTime} className="block py-2 px-4 border-2"/>
-          </div>
-          <div className="justify-center-auto mr-8">
-            <label className="block font-medium">Email</label>
-            <input type="email" value={email} onChange={updateEmail} className="block py-2 px-4 border-2"/>
-          </div>
-          <div className="justify-center-auto mr-8">
-          <button className="bg-black text-white py-3 px-5 rounded-lg mt-6">Book A Table</button>
-          </div>
-        </div>
-      </form>
-    </div>
-  )
-}
+	function updateTime(e: React.FormEvent<HTMLInputElement>) {
+		setTime(e.currentTarget.value);
+	}
 
-export default BookTable
+	function updateEmail(e: React.FormEvent<HTMLInputElement>) {
+		setEmail(e.currentTarget.value);
+	}
+
+	function submitForm(e: React.FormEvent) {
+		e.preventDefault();
+		if (!dateValid) {
+			alert('Booking date should be at least one day in advance');
+		} else if (!timeValid) {
+			alert('Booking time should be between 1.00 PM and 10.00 PM');
+		} else if (!emailValid || !email) {
+			alert('Please provide a valid email before booking a table');
+		} else {
+			dispatch(
+				bookTable({
+					date,
+					email,
+					time,
+				})
+			);
+		}
+	}
+
+	return (
+		<div className="mb-12 overflow-hidden">
+			<h2 className="mb-4 font-medium text-lg">Book A Table</h2>
+			<form
+				className="md:w-11/12  bg-white rounded-md shadow-lg py-4"
+				action="#"
+				onSubmit={submitForm}
+			>
+				{!dateValid && (
+					<pre className="p-2 text-center text-sm text-red-500">
+						Your booking date should be <br className="md:hidden block" /> at least one day in
+						advance.
+					</pre>
+				)}
+				{!timeValid && (
+					<pre className="p-2 text-center text-sm text-red-500">
+						Booking time should be between <br className="md:hidden block" /> 1.00 PM and 10.00 PM
+						EAT
+					</pre>
+				)}
+				{!emailValid && (
+					<pre className="p-2 text-center text-sm text-red-500">
+						Please provide a valid email address
+					</pre>
+				)}
+				<div className="md:flex justify-center p-4">
+					<div className="justify-center-auto md:mr-8">
+						<label className="block font-medium">Date</label>
+						<input
+							type="date"
+							value={date}
+							onChange={updateDate}
+							className="block py-2 px-4 border-2 w-full"
+						/>
+					</div>
+					<div className="justify-center-auto md:mr-8">
+						<label className="block font-medium">Time</label>
+						<input
+							type="time"
+							value={time}
+							onChange={updateTime}
+							className="block py-2 px-4 border-2 w-full"
+						/>
+					</div>
+					<div className="justify-center-auto md:mr-8">
+						<label className="block font-medium">Email</label>
+						<input
+							type="email"
+							value={email}
+							onChange={updateEmail}
+							className="block py-2 px-4 border-2 w-full"
+						/>
+					</div>
+					<div className="justify-center-auto md:mr-8">
+						<button className="bg-black text-white py-3 px-5 rounded-lg mt-6 md:w-auto w-full">
+							Book A Table
+						</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	);
+};
+
+export default BookTable;
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,29 +1,34 @@
-import { Link } from "react-router-dom"
-import BookTable from "./bookTable"
+import { Link } from 'react-router-dom';
+import BookTable from './bookTable';
 
-const LandingPage = () =>{
-  return (
-      <div className="py-2 px-28 mb-8">
-        <div className="flex">
-          <div className="flex-auto">
-            <h3 className="font-medium my-2">EASY WAY TO ORDER YOUR FOOD</h3>
-            <h1 className="text-6xl font-bold">
-              Choose Healthy <br/>& Fresh Food
-            </h1>
-            <p className="my-2">
-              Just confirm your order and enjoy our delicious fastest delivery.
-            </p>
-            <Link to="/menu">
-              <button className="bg-black text-white py-3 px-5 rounded-lg my-2">Order Now</button>
-            </Link>
-          </div>
-          <div className="flex-auto">
-            <img className="w-80 mx-auto block" src="/assets/img/food-landing.png"/>
-          </div>
-        </div>
-        <BookTable />
-      </div>
-  )
-}
+const LandingPage = () => {
+	return (
+		<div className="md:py-2 py-8 md:px-28 px-8 mb-8">
+			<div className="md:flex">
+				<div className="md:flex-auto mb-8 md:mb-0">
+					<h3 className="font-medium my-2">EASY WAY TO ORDER YOUR FOOD</h3>
+					<h1 className="md:text-6xl text-4xl font-bold">
+						Choose Healthy <br />& Fresh Food
+					</h1>
+					<p className="my-2">Just confirm your order and enjoy our delicious fastest delivery.</p>
+					<Link to="/menu" className="md:block hidden">
+						<button className="bg-black text-white py-3 px-5 rounded-lg my-2 w-full md:w-auto">
+							Order Now
+						</button>
+					</Link>
+				</div>
+				<div className="flex-auto">
+					<img className="md:w-80 w-60 mx-auto block" src="/assets/img/food-landing.png" />
+				</div>
+				<Link to="/menu" className="block md:hidden my-6">
+					<button className="bg-black text-white py-3 px-5 rounded-lg my-2 w-full md:w-auto">
+						Order Now
+					</button>
+				</Link>
+			</div>
+			<BookTable />
+		</div>
+	);
+};
 
-export default LandingPage
+export default LandingPage;
